82bbbbd431ad24d72d93291265e1fbf04698f849
Sumit Prasad <sumitprasad585@gmail.com>
2024-06-27 23:24:32 +0100

    connect to database

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 dotenv.config({ path: './config.private.env' });
+const mongoose = require('mongoose');
 
 const app = require('./app');
+
+let DATABASE = process.env.DATABASE_URL;
+DATABASE = DATABASE.replace('<username>', process.env.DATABASE_USERNAME);
+DATABASE = DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+DATABASE = DATABASE.replace('<dbname>', process.env.DBNAME);
+
+mongoose.connect(DATABASE, {
+  useNewUrlParser: true,
+})
+  .then(con => {
+    // console.log(con);
+    console.log('Database connection successful...');
+  })
+  .catch(err => {
+    console.log(err);
+    console.log('Error connecting to database');
+    console.log('Shutting down...');
+    process.exit(1);
+  });
 
 const PORT = 5858;
 const server = app.listen(PORT, () => {
