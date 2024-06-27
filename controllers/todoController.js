7e3f73a8576ac8059c284e5007b6315fcf8bd60a
Sumit Prasad <sumitprasad585@gmail.com>
2024-06-28 00:33:48 +0100

    CRUD operations on todos

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,25 +1,62 @@
+const Todo = require("../models/todoModel");
 const catchAsync = require("../utils/catchAsync");
 
 const getAllTodos = catchAsync(async (req, res, next) => {
+  const todos = await Todo.find({});
   res.status(200).json({
-    status: 'success'
+    status: 'success',
+    length: todos.length,
+    data: {
+      todos
+    }
   });
 });
 
 const getTodo = catchAsync(async (req, res, next) => {
-
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'todo not found'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      todo
+    }
+  });
 });
 
 const createTodo = catchAsync(async (req, res, next) => {
-
+  const todo = await Todo.create(req.body);
+  return res.status(201).json({
+    status: 'success',
+    data: {
+      todo
+    }
+  });
 });
 
 const updateTodo = catchAsync(async (req, res, next) => {
-
+  const updated = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      todo: updated
+    }
+  });
 });
 
 const deleteTodo = catchAsync(async (req, res, next) => {
-
+  await Todo.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
 });
 
 module.exports = {
---
+++ models/todoModel.js
@@ -0,0 +1,95 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'a todo must have a name'],
+    unique: true
+  },
+  summary: {
+    type: String,
+    required: [true, 'a todo must have a summary'],
+    unique: true
+  },
+  description: {
+    type: String,
+    required: [true, 'a todo must a description']
+  },
+  priority: {
+    type: String,
+    enum: {
+      values: ['negligible', 'low', 'medium', 'high', 'critical'],
+      message: 'Invalid value for priority: {VALUE}'
+    },
+    default: 'low'
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ['draft', 'inprogress', 'blocked', 'done', 'plannedforfuture'],
+      message: 'Invalid value for status: {VALUE}'
+    },
+    default: 'draft'
+  },
+  listScore: {
+    type: Number
+  },
+  labels: {
+    type: [String]
+  },
+  comments: {
+    type: [String]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  slug: {
+    type: String
+  }
+});
+
+/** Assign a list score based on priority */
+function assignListScore(document, next) {
+  switch (document.priority) {
+    case 'negligible':
+      document.listScore = 0;
+      break;
+    case 'low':
+      document.listScore = 1;
+      break;
+    case 'medium':
+      document.listScore = 2;
+      break;
+    case 'high':
+      document.listScore = 3;
+      break;
+    case 'critical':
+      document.listScore = 4;
+      break;
+    default:
+      document.listScore = 1;
+  }
+
+  next();
+}
+
+/** Add a pre save hook to assign a list score based on priority */
+todoSchema.pre('save', function (next) {
+  /** 'this' refers to document here */
+  assignListScore(this, next);
+});
+
+/** Add a pre update hook to assign a list score based on priority */
+todoSchema.pre('findOneAndUpdate', function (next) {
+  /** 'this' refers to query object here */
+  let update = this.getUpdate();
+  assignListScore(update, next);
+});
+
+todoSchema.index({ name: 1 }, { unique: true });
+todoSchema.index({ summary: 1 }, { unique: true });
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = Todo;
