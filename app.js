ee09172773db4337d85c2c799b3548d8f4de0368
Sumit Prasad <sumitprasad585@gmail.com>
2024-06-29 15:51:40 +0100

    add rate limiter and other security related middlewares

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ const todoRouter = require('./routes/todoRoutes');
 const userRouter = require('./routes/userRoutes');
 const { globalErrorHandler } = require('./controllers/errorController');
 const AppError = require('./utils/appError');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
 if (process.env.NODE_ENV === 'development')
   app.use(morgan('dev'));
 
+app.use(helmet());
+
+/** Use the rate limiter */
+const limiter = rateLimit({
+  windowMs: 1000 * 60 * 60 * 1,
+  limit: 100
+});
+app.use('/api', limiter);
+
 // app.use(express.json());
 app.use(bodyParser.json());
+app.use(mongoSanitize());
+app.use(hpp({
+  whitelist: ['listScore']
+}));
 
-/* Mount the routers */
+/** Mount the routers */
 app.use('/api/v1/todos', todoRouter);
 app.use('/api/v1/users', userRouter);
 
